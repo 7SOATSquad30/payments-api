dcd0e291a9182cd451eb8aad95857b751c4170c2
Murilo Kakazu <yuri.kakazu@gmail.com>
2024-11-20 16:51:00 -0300

    fix: remove init.sql

--- init.sql
+++
@@ -1,1 +0,0 @@
-create table tb_payment (amount float(53) not null check ((amount>=0) and (amount<=10000)), created_at TIMESTAMP WITHOUT TIME ZONE, deleted_at TIMESTAMP WITHOUT TIME ZONE, order_id bigint not null, updated_at TIMESTAMP WITHOUT TIME ZONE, status varchar(255) check (status in ('PENDING','REJECTED','COLLECTED')), primary key (order_id));
